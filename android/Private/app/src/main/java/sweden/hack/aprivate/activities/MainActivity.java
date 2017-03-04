f17cf2c457c303cf05021417dda6931717ba1670
Markus <karl.markus.larsson@gmail.com>
2017-03-04 11:45:04 +0100

    moved packages

--- android/Private/app/src/main/java/sweden/hack/aprivate/activities/MainActivity.java
+++ android/Private/app/src/main/java/sweden/hack/aprivate/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package sweden.hack.aprivate;
+package sweden.hack.aprivate.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import sweden.hack.aprivate.R;
 
 public class MainActivity extends AppCompatActivity {
 
