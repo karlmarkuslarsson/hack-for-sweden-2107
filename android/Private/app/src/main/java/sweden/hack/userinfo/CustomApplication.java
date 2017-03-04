2e4ab58277deffd323b2550eb959974f245c79ec
Markus <karl.markus.larsson@gmail.com>
2017-03-04 12:09:42 +0100

    added custom application

---
+++ android/Private/app/src/main/java/sweden/hack/userinfo/CustomApplication.java
@@ -0,0 +1,14 @@
+package sweden.hack.userinfo;
+
+import android.app.Application;
+
+import net.danlew.android.joda.JodaTimeAndroid;
+
+public class CustomApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        JodaTimeAndroid.init(this);
+    }
+
+}
