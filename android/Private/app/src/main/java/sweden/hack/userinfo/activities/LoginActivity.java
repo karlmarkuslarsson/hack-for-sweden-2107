4857600b19f6132cf646581cc0675e90fb3b5f3b
Sofie Svensson <sofie.svensson@netlight.com>
2017-03-05 18:17:35 +0100

    added activity login

---
+++ android/Private/app/src/main/java/sweden/hack/userinfo/activities/LoginActivity.java
@@ -0,0 +1,18 @@
+package sweden.hack.userinfo.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import sweden.hack.userinfo.R;
+
+/**
+ * Created by sosv on 04/03/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
