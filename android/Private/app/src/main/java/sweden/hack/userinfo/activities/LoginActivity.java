c8816fe23a53df9f8348f134e91a4a2717e83e3c
Markus <karl.markus.larsson@gmail.com>
2017-03-05 21:39:05 +0100

    added checks for input length of person number

--- android/Private/app/src/main/java/sweden/hack/userinfo/activities/LoginActivity.java
+++ android/Private/app/src/main/java/sweden/hack/userinfo/activities/LoginActivity.java
@@ -3,8 +3,11 @@ package sweden.hack.userinfo.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import sweden.hack.userinfo.R;
 import sweden.hack.userinfo.helpers.DataHelper;
@@ -32,14 +35,34 @@ public class LoginActivity extends AppCompatActivity {
                 String input = mInputField.getText().toString();
 
                 // TODO: condition for input field
-                if (true) {
+                if (checkIfInputIsCorrect(input)) {
                     DataHelper.setUserPersonNumber(input);
                     startMainActivity();
-                } else {
-
                 }
             }
         });
+
+        mInputField.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    mLoginButton.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private boolean checkIfInputIsCorrect(String input) {
+        if (input.length() != 10) {
+            mInputField.setError(getString(R.string.person_number_minimum_length_error));
+            return false;
+        }
+
+        // add more checks..
+
+        return true;
     }
 
     private void startMainActivity() {
