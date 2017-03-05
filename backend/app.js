ef1af3c76e0d8311146a8d2ddcd248308bc81932
Filip Lindqvist <buggfille@gmail.com>
2017-03-06 00:04:40 +0100

    Added scb test API call

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,21 @@
 var express = require('express')
 var app = express()
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('hello hack for sweden!')
-});
+var scb = require('./scb');
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!')
+var PORT = process.env.NODE_PORT || 3000
+
+app.get('/', function (req, res) {
+  	res.send('hello hack for sweden!')
+})
+
+app.get('/scb', function(req, res) {
+	scb.get(function(err, resp, body) {
+		console.log(resp);
+		res.send(resp.body);
+	});
+})
+
+app.listen(PORT, function () {
+  console.log('App listening on port', PORT)
 })
