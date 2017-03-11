1a0bba45262d6b344b1c12fd5adf3b1b8f3ccc26
Sofie Svensson <sofie.svensson@netlight.com>
2017-03-11 17:04:27 +0100

    fixed problem

--- android/app/src/main/java/sweden/hack/userinfo/models/phrases/Phrases.java
+++ android/app/src/main/java/sweden/hack/userinfo/models/phrases/Phrases.java
@@ -1,6 +1,8 @@
 package sweden.hack.userinfo.models.phrases;
 
-public class Phrases {
+import sweden.hack.userinfo.models.CardComponent;
+
+public class Phrases extends CardComponent {
 
     public String getPhrases() {
         return "One cup of home brewed coffee please - En kopp hembryggt kaffe tack";
