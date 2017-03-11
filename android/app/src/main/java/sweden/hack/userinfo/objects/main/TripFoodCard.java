985c1cade6aaba6e5c9e1901ff19535656a51542
Markus <karl.markus.larsson@gmail.com>
2017-03-11 21:50:40 +0100

    mapped trip models to cards

--- android/app/src/main/java/sweden/hack/userinfo/objects/main/TripFoodCard.java
+++ android/app/src/main/java/sweden/hack/userinfo/objects/main/TripFoodCard.java
@@ -1,10 +1,22 @@
 package sweden.hack.userinfo.objects.main;
 
+import sweden.hack.userinfo.models.myTrip.MyTripEvent;
 import sweden.hack.userinfo.objects.main.base.MainCard;
 
 public class TripFoodCard extends MainCard {
+
+    private final MyTripEvent mTripEvent;
+
+    public TripFoodCard (MyTripEvent tripEvent) {
+        mTripEvent = tripEvent;
+    }
+
     @Override
     public int getViewType() {
         return MainCard.TYPE_TRIP_FOOD;
+    }
+
+    public MyTripEvent getTripEvent() {
+        return mTripEvent;
     }
 }
--- android/app/src/main/java/sweden/hack/userinfo/objects/main/TripPlaceCard.java
+++ android/app/src/main/java/sweden/hack/userinfo/objects/main/TripPlaceCard.java
@@ -1,10 +1,22 @@
 package sweden.hack.userinfo.objects.main;
 
+import sweden.hack.userinfo.models.myTrip.MyTripRestaurant;
 import sweden.hack.userinfo.objects.main.base.MainCard;
 
 public class TripPlaceCard extends MainCard {
+
+    private final MyTripRestaurant mTripRestaurant;
+
+    public TripPlaceCard (MyTripRestaurant tripRestaurant) {
+        mTripRestaurant = tripRestaurant;
+    }
+
     @Override
     public int getViewType() {
         return MainCard.TYPE_TRIP_PLACE;
+    }
+
+    public MyTripRestaurant getTripRestaurant() {
+        return mTripRestaurant;
     }
 }
