6980cf2c2c6711dd7d040cb1a490663e065c9c18
Filip Lindqvist <buggfille@gmail.com>
2017-03-11 14:25:03 +0100

    remove gender, update start

--- android/app/src/main/java/sweden/hack/userinfo/activities/StartActivity.java
+++ android/app/src/main/java/sweden/hack/userinfo/activities/StartActivity.java
@@ -3,27 +3,21 @@ package sweden.hack.userinfo.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import sweden.hack.userinfo.R;
 import sweden.hack.userinfo.helpers.DataHelper;
 
 public class StartActivity extends AppCompatActivity {
-    private Button mLoginButton;
-    private EditText mPersonNumberField;
-    private EditText mFirstNameField;
-    private EditText mSurnameField;
+
+    private Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (!userSignedIn()) {
-            setContentView(R.layout.activity_login);
+            setContentView(R.layout.activity_start);
             initViews();
             setupCallbacks();
         }
@@ -39,51 +33,16 @@ public class StartActivity extends AppCompatActivity {
     }
 
     private void setupCallbacks() {
-        mLoginButton.setOnClickListener(new View.OnClickListener() {
+        mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String personNumber = mPersonNumberField.getText().toString();
-                String firstName = mFirstNameField.getText().toString();
-                String surname = mSurnameField.getText().toString();
-
-                // TODO: condition for input field
-                if (checkIfInputIsCorrect(personNumber, firstName, surname)) {
-                    DataHelper.setUserPersonNumber(personNumber);
-                    DataHelper.setFirstName(firstName);
-                    DataHelper.setSurname(surname);
-                    startMainActivity();
-                }
-            }
-        });
-
-        mPersonNumberField.setOnEditorActionListener(new EditText.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    mLoginButton.performClick();
-                    return true;
-                }
-                return false;
+                startMainActivity();
             }
         });
     }
 
     private boolean checkIfInputIsCorrect(String personNumber, String firstName, String surname) {
         boolean success = true;
-        if (personNumber.length() != 10) {
-            mPersonNumberField.setError(getString(R.string.person_number_minimum_length_error));
-            success = false;
-        }
-
-        if (firstName.isEmpty()) {
-            mFirstNameField.setError(getString(R.string.name_length_error));
-            success = false;
-        }
-
-        if (surname.isEmpty()) {
-            mSurnameField.setError(getString(R.string.name_length_error));
-            success = false;
-        }
 
         // add more checks..
 
@@ -97,10 +56,7 @@ public class StartActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        mLoginButton = (Button) findViewById(R.id.activity_login_button);
-        mPersonNumberField = (EditText) findViewById(R.id.activity_login_input);
-        mFirstNameField = (EditText) findViewById(R.id.activity_login_first_name);
-        mSurnameField = (EditText) findViewById(R.id.activity_login_surname);
+        mStartButton = (Button) findViewById(R.id.activity_start_button);
     }
 
 }
--- android/app/src/main/java/sweden/hack/userinfo/objects/main/base/MainCard.java
+++ android/app/src/main/java/sweden/hack/userinfo/objects/main/base/MainCard.java
@@ -4,7 +4,7 @@ package sweden.hack.userinfo.objects.main.base;
  * All cards mot extend MainCard
  */
 public abstract class MainCard {
-    public final static int TYPE_GENDER = 0;
+
     public final static int TYPE_AGE = 1;
     public static final int TYPE_POPULATION = 2;
     public static final int TYPE_INCOME = 3;
