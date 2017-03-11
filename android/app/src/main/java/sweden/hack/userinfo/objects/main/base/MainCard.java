8e922ba8c83783316e78c30613cc40201488eaff
Markus <karl.markus.larsson@gmail.com>
2017-03-11 21:43:10 +0100

    added trip transportation card

--- android/app/src/main/java/sweden/hack/userinfo/objects/main/base/MainCard.java
+++ android/app/src/main/java/sweden/hack/userinfo/objects/main/base/MainCard.java
@@ -18,6 +18,7 @@ public abstract class MainCard {
     public static final int TYPE_SL_AIRPORT_TRIP = 11;
     public static final int TYPE_TRIP_PLACE = 12;
     public static final int TYPE_TRIP_FOOD = 13;
+    public static final int TYPE_TRIP_TRANSPORTATION = 14;
 
     public abstract int getViewType();
 }
---
+++ android/app/src/main/java/sweden/hack/userinfo/viewholders/main/TripTransportationViewHolder.java
@@ -0,0 +1,25 @@
+package sweden.hack.userinfo.viewholders.main;
+
+import android.view.View;
+import android.widget.TextView;
+
+import sweden.hack.userinfo.R;
+import sweden.hack.userinfo.listeners.MainCardListener;
+import sweden.hack.userinfo.objects.main.TripTransportationCard;
+
+public class TripTransportationViewHolder extends MainViewHolder<TripTransportationCard> {
+
+    private final TextView mTransportationType;
+    private final TextView mTransportationTime;
+
+    public TripTransportationViewHolder(View itemView) {
+        super(itemView);
+        mTransportationType = (TextView) itemView.findViewById(R.id.transportation_type);
+        mTransportationTime = (TextView) itemView.findViewById(R.id.transportation_time);
+    }
+
+    @Override
+    public void init(TripTransportationCard card, MainCardListener listener) {
+
+    }
+}
