68a1967d28c2014522c68d7c875405049e33ea59
Markus <karl.markus.larsson@gmail.com>
2017-03-11 17:01:57 +0100

    Updated design

--- android/app/src/main/java/sweden/hack/userinfo/models/phrases/Phrases.java
+++ android/app/src/main/java/sweden/hack/userinfo/models/phrases/Phrases.java
@@ -1,21 +1,8 @@
 package sweden.hack.userinfo.models.phrases;
 
-import com.google.gson.annotations.SerializedName;
-
-import java.util.List;
-
-import sweden.hack.userinfo.models.CardComponent;
-
-public class Phrases extends CardComponent {
-
-    @SerializedName("phrases")
-    private List<String> mPhrases;
+public class Phrases {
 
     public String getPhrases() {
-        StringBuilder sb = new StringBuilder();
-        for (String str : mPhrases) {
-            sb.append(str).append("\n");
-        }
-        return sb.toString();
+        return "One cup of home brewed coffee please - En kopp hembryggt kaffe tack";
     }
 }
