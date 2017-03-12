0e07ef55d6ad8121c61ec6abe58af9641dfef722
Markus <karl.markus.larsson@gmail.com>
2017-03-12 06:03:42 +0100

    redesign phrases card

--- android/app/src/main/java/sweden/hack/userinfo/viewholders/main/PhrasesViewHolder.java
+++ android/app/src/main/java/sweden/hack/userinfo/viewholders/main/PhrasesViewHolder.java
@@ -22,13 +22,27 @@ public class PhrasesViewHolder extends MainViewHolder<PhrasesCard> {
     public void init(PhrasesCard card, MainCardListener listener) {
         mContent.removeAllViews();
         LayoutInflater inflater = LayoutInflater.from(itemView.getContext());
-        for (Phrase phrase : card.getPhrases().getPhrases()) {
-            View view = inflater.inflate(R.layout.row_language, mContent, false);
-            TextView swe = (TextView) view.findViewById(R.id.swe);
-            TextView eng = (TextView) view.findViewById(R.id.eng);
-            swe.setText(phrase.getSwe());
-            eng.setText(phrase.getEng());
-            mContent.addView(view);
+        for (int i = 0; i < card.getPhrases().getPhrases().size(); i++) {
+            LinearLayout linearLayout = new LinearLayout(itemView.getContext());
+            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            linearLayout.setWeightSum(2);
+
+            addView(linearLayout, card.getPhrases().getPhrases().get(i), inflater);
+            i++;
+            if (i < card.getPhrases().getPhrases().size()) {
+                addView(linearLayout, card.getPhrases().getPhrases().get(i), inflater);
+            }
+
+            mContent.addView(linearLayout);
         }
+    }
+
+    private void addView(LinearLayout linearLayout, Phrase phrase, LayoutInflater inflater) {
+        View view = inflater.inflate(R.layout.row_language, mContent, false);
+        TextView swe = (TextView) view.findViewById(R.id.swe);
+        TextView eng = (TextView) view.findViewById(R.id.eng);
+        swe.setText(phrase.getSwe());
+        eng.setText(phrase.getEng());
+        linearLayout.addView(view);
     }
 }
