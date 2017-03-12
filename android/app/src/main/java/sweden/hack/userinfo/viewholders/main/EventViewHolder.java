5f989cecf07e5fc557d020d769e049ffe85771dc
Markus <karl.markus.larsson@gmail.com>
2017-03-12 07:23:24 +0100

    remove image in event if load fail

--- android/app/src/main/java/sweden/hack/userinfo/viewholders/main/EventViewHolder.java
+++ android/app/src/main/java/sweden/hack/userinfo/viewholders/main/EventViewHolder.java
@@ -7,6 +7,9 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import sweden.hack.userinfo.R;
 import sweden.hack.userinfo.listeners.MainCardListener;
@@ -44,11 +47,28 @@ public class EventViewHolder extends MainViewHolder<EventCard> {
         mTextLocation.setText(Html.fromHtml(
                 location == null ? "" : location
         ));
-        Glide.with(itemView.getContext())
-                .load(card.getEvent().getImage())
-                .diskCacheStrategy(DiskCacheStrategy.RESULT)
-                .crossFade()
-                .fallback(android.R.drawable.ic_input_get)
-                .into(mImage);
+        if (mImage != null) {
+            mImage.setVisibility(View.VISIBLE);
+            Glide.with(itemView.getContext())
+                    .load(card.getEvent().getImage())
+                    .diskCacheStrategy(DiskCacheStrategy.RESULT)
+                    .crossFade()
+                    .fallback(android.R.drawable.ic_input_get)
+                    .listener(new RequestListener<String, GlideDrawable>() {
+                        @Override
+                        public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                            mImage.setVisibility(View.GONE);
+                            return true;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                            return false;
+                        }
+                    })
+                    .into(mImage);
+        } else {
+            mImage.setVisibility(View.GONE);
+        }
     }
 }
