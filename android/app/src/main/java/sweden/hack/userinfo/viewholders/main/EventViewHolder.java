1be260b96e541ae1cf39a5bff0ea764f4abf96e4
Filip Lindqvist <buggfille@gmail.com>
2017-03-12 06:41:44 +0100

    Add location

--- android/app/src/main/java/sweden/hack/userinfo/viewholders/main/EventViewHolder.java
+++ android/app/src/main/java/sweden/hack/userinfo/viewholders/main/EventViewHolder.java
@@ -17,20 +17,32 @@ public class EventViewHolder extends MainViewHolder<EventCard> {
     private final ImageView mImage;
     private final TextView mTextTitle;
     private final TextView mTextDescription;
+    private final TextView mTextLocation;
 
     public EventViewHolder(View root) {
         super(root);
         mImage = (ImageView) itemView.findViewById(R.id.image);
         mTextTitle = (TextView) itemView.findViewById(R.id.title);
         mTextDescription = (TextView) itemView.findViewById(R.id.description);
+        mTextLocation = (TextView) itemView.findViewById(R.id.location);
     }
 
     @Override
     public void init(EventCard card, MainCardListener listener) {
-        mTextTitle.setText(card.getEvent().getTitle());
+        String title = card.getEvent().getTitle();
+        mTextTitle.setText(
+                Html.fromHtml(
+                        title == null ? "" : title
+                )
+        );
         String description = card.getEvent().getDescription();
         mTextDescription.setText(Html.fromHtml(
                 description == null ? "" : description
+        ));
+
+        String location = card.getEvent().getLocation();
+        mTextLocation.setText(Html.fromHtml(
+                location == null ? "" : location
         ));
         Glide.with(itemView.getContext())
                 .load(card.getEvent().getImage())
