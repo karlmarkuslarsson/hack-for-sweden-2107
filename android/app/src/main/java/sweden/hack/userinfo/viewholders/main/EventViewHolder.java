d83048e155fbd021c1cdfdbbfc50ad5c5a51c3a4
Filip Lindqvist <buggfille@gmail.com>
2017-03-12 02:25:50 +0100

    Event update

--- android/app/src/main/java/sweden/hack/userinfo/viewholders/main/EventViewHolder.java
+++ android/app/src/main/java/sweden/hack/userinfo/viewholders/main/EventViewHolder.java
@@ -1,29 +1,42 @@
 package sweden.hack.userinfo.viewholders.main;
 
+import android.text.Html;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import sweden.hack.userinfo.R;
 import sweden.hack.userinfo.listeners.MainCardListener;
 import sweden.hack.userinfo.objects.main.EventCard;
 
-/**
- * Created by sosv on 11/03/17.
- */
-
 public class EventViewHolder extends MainViewHolder<EventCard> {
+
+    private final ImageView mImage;
     private final TextView mTextTitle;
-    private final TextView mTextDate;
+    private final TextView mTextDescription;
 
     public EventViewHolder(View root) {
         super(root);
-        mTextTitle = (TextView) itemView.findViewById(R.id.event_title);
-        mTextDate = (TextView) itemView.findViewById(R.id.event_date);
+        mImage = (ImageView) itemView.findViewById(R.id.image);
+        mTextTitle = (TextView) itemView.findViewById(R.id.title);
+        mTextDescription = (TextView) itemView.findViewById(R.id.description);
     }
 
     @Override
     public void init(EventCard card, MainCardListener listener) {
         mTextTitle.setText(card.getEvent().getTitle());
-        mTextDate.setText(card.getEvent().getDescription());
+        String description = card.getEvent().getDescription();
+        mTextDescription.setText(Html.fromHtml(
+                description == null ? "" : description
+        ));
+        Glide.with(itemView.getContext())
+                .load(card.getEvent().getImage())
+                .diskCacheStrategy(DiskCacheStrategy.RESULT)
+                .crossFade()
+                .fallback(android.R.drawable.ic_input_get)
+                .into(mImage);
     }
 }
