25273afe708ccc37994540382f305e8b3b655978
Filip Lindqvist <buggfille@gmail.com>
2017-03-12 04:34:23 +0100

    Airport + currency switch...

---
+++ android/app/src/main/java/sweden/hack/userinfo/viewholders/main/SLAirportTripViewHolder.java
@@ -0,0 +1,19 @@
+package sweden.hack.userinfo.viewholders.main;
+
+import android.view.View;
+
+import sweden.hack.userinfo.listeners.MainCardListener;
+import sweden.hack.userinfo.objects.main.SLAirportCard;
+
+public class SLAirportTripViewHolder extends MainViewHolder<SLAirportCard> {
+    public SLAirportTripViewHolder(View root) {
+        super(root);
+
+
+    }
+
+    @Override
+    public void init(SLAirportCard card, MainCardListener listener) {
+
+    }
+}
