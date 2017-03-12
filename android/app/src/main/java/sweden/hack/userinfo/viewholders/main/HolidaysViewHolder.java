737c6be3ae42f108ee41e47d2a76fe7c4d534de2
Markus <karl.markus.larsson@gmail.com>
2017-03-12 06:27:02 +0100

    making swedish holidays compact

--- android/app/src/main/java/sweden/hack/userinfo/viewholders/main/HolidaysViewHolder.java
+++ android/app/src/main/java/sweden/hack/userinfo/viewholders/main/HolidaysViewHolder.java
@@ -3,6 +3,7 @@ package sweden.hack.userinfo.viewholders.main;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -12,7 +13,7 @@ import sweden.hack.userinfo.listeners.MainCardListener;
 import sweden.hack.userinfo.models.holdays.Holiday;
 import sweden.hack.userinfo.objects.main.HolidaysCard;
 
-public class HolidaysViewHolder extends MainViewHolder<HolidaysCard>{
+public class HolidaysViewHolder extends MainViewHolder<HolidaysCard> {
 
     private final ViewGroup mContent;
 
@@ -26,12 +27,26 @@ public class HolidaysViewHolder extends MainViewHolder<HolidaysCard>{
         LayoutInflater inflater = LayoutInflater.from(itemView.getContext());
         List<Holiday> holidays = card.getHolidays().getHolidays();
         mContent.removeAllViews();
-        for (Holiday holiday : holidays) {
-            View view = inflater.inflate(R.layout.card_holidays_part, mContent, false);
-            TextView.class.cast(view.findViewById(R.id.text1)).setText(holiday.getName());
-            TextView.class.cast(view.findViewById(R.id.text2)).setText(holiday.getWeekday());
-            TextView.class.cast(view.findViewById(R.id.text3)).setText(holiday.getDate());
-            mContent.addView(view);
+        for (int i = 0; i < holidays.size(); i++) {
+            Holiday holiday = holidays.get(i);
+            LinearLayout linearLayout = new LinearLayout(itemView.getContext());
+            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            linearLayout.setWeightSum(2);
+            addView(linearLayout, holiday, inflater);
+            i++;
+            if (i < holidays.size()) {
+                addView(linearLayout, holidays.get(i), inflater);
+            }
+
+            mContent.addView(linearLayout);
         }
+    }
+
+    private void addView(LinearLayout linearLayout, Holiday holiday, LayoutInflater inflater) {
+        View view = inflater.inflate(R.layout.card_holidays_part, linearLayout, false);
+        TextView.class.cast(view.findViewById(R.id.text1)).setText(holiday.getName());
+        TextView.class.cast(view.findViewById(R.id.text2)).setText(holiday.getWeekday());
+        TextView.class.cast(view.findViewById(R.id.text3)).setText(holiday.getDate());
+        linearLayout.addView(view);
     }
 }
