aaa2d3c2099dcf53e94dca3ef6be3c3f51c562ec
Markus Larsson <karl.markus.larsson@gmail.com>
2017-05-28 17:48:32 +0200

    removed unused import

--- android/app/src/main/java/com/welcome/to/sweden/dialogs/EventDialog.java
+++ android/app/src/main/java/com/welcome/to/sweden/dialogs/EventDialog.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
